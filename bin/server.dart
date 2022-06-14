6a251b60ff0e652e20bc3d044dd62107ac74b10e
Jeho <xpwmaosldk@gmail.com>
2022-06-14 20:09:40 +0900

    add dart project

---
+++ bin/server.dart
@@ -0,0 +1,13 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+
+void main() {
+  final _handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(createStaticHandler(
+            'example/files',
+            defaultDocument: 'index.html',
+          ));
+
+  io.serve(_handler, 'localhost', 8080);
+}
